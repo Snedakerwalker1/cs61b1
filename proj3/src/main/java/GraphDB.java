94fd309b035f83143ae3c565a411f8fbdc7fa3b5
Walker Snedaker <wsnedaker@berkeley.edu>
2017-04-18 22:50:09 -0700

    made a greaph

--- proj3/src/main/java/GraphDB.java
+++ proj3/src/main/java/GraphDB.java
@@ -1,3 +1,4 @@
+import com.sun.javafx.geom.Edge;
 import org.xml.sax.SAXException;
 
 import java.io.File;
@@ -6,6 +7,9 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * Graph for storing all of the intersection (vertex) and road (edge) information.
@@ -17,15 +21,54 @@ import java.util.ArrayList;
  * @author Alan Yao, Josh Hug
  */
 public class GraphDB {
+    private HashMap<Long, Node> nodeMap;
+    private HashMap<Long, HashSet<Edge>> edgeMap;
+    private List<Long> nodeList;
     /** Your instance variables for storing the graph. You should consider
      * creating helper classes, e.g. Node, Edge, etc. */
+    private class Node {
+        long id;
+        double lon;
+        double lat;
+        String name = "";
+        List<Long> adjacent;
 
+        Node(long id1, double lon1, double lat1) {
+            this.id = id1;
+            this.lon = lon1;
+            this.lat = lat1;
+            this.adjacent = new ArrayList<>();
+        }
+        void setName(String name1) {
+            this.name = name1;
+        }
+    }
+    private class Edge {
+        long edgeId;
+        Node nodes;
+        String maxSpeed = "";
+        String name = "";
+
+        Edge(long id, Node node) {
+            this.edgeId = id;
+            this.nodes = node;
+        }
+        void setMaxSpeed(String speed) {
+            this.maxSpeed = speed;
+        }
+        void setName(String name1) {
+            this.name = name1;
+        }
+    }
     /**
      * Example constructor shows how to create and start an XML parser.
      * You do not need to modify this constructor, but you're welcome to do so.
      * @param dbPath Path to the XML file to be parsed.
      */
     public GraphDB(String dbPath) {
+        this.nodeList = new ArrayList<>();
+        this.nodeMap = new HashMap<>();
+        this.edgeMap = new HashMap<>();
         try {
             File inputFile = new File(dbPath);
             SAXParserFactory factory = SAXParserFactory.newInstance();
@@ -52,39 +95,106 @@ public class GraphDB {
      *  While this does not guarantee that any two nodes in the remaining graph are connected,
      *  we can reasonably assume this since typically roads are connected.
      */
+    public void addNode(long id, double lon, double lat) {
+        Node node = new Node(id, lon, lat);
+        nodeMap.put(id, node);
+        nodeList.add(id);
+    }
+    public void addEdge(long edge1, List<Long> ids, String name, String speed) {
+        for (long id1: ids) {
+            Node node1 = nodeMap.get(id1);
+            Edge edge = new Edge(edge1, node1);
+            edge.setName(name);
+            edge.setMaxSpeed(speed);
+            if (edgeMap.containsKey(edge1)) {
+                edgeMap.get(edge1).add(edge);
+            } else {
+                HashSet<Edge> edgeList = new HashSet<>();
+                edgeList.add(edge);
+                edgeMap.put(edge1, edgeList);
+            }
+            for (Edge edg : edgeMap.get(edge1)) {
+                if (edg.nodes.id != id1) {
+                    edg.nodes.adjacent.add(id1);
+                }
+            }
+        }
+    }
+    public void addNodeName(long id, String name1) {
+        if (nodeMap.containsKey(id)) {
+            nodeMap.get(id).setName(name1);
+        }
+    }
+    public void removeNode(long v) {
+        nodeList.remove(v);
+        nodeMap.remove(v);
+    }
+
     private void clean() {
-        // TODO: Your code here.
+        for (int i = 0; i < nodeList.size(); i += 1) {
+            long v = nodeList.get(i);
+            Node node = getNode(v);
+            if (node.adjacent.isEmpty()) {
+                removeNode(v);
+            }
+        }
     }
 
     /** Returns an iterable of all vertex IDs in the graph. */
     Iterable<Long> vertices() {
         //YOUR CODE HERE, this currently returns only an empty list.
-        return new ArrayList<Long>();
+        return this.nodeList;
+    }
+    Node getNode(long v) {
+        if (nodeMap.containsKey(v)) {
+            return nodeMap.get(v);
+        }
+        return null;
     }
 
     /** Returns ids of all vertices adjacent to v. */
     Iterable<Long> adjacent(long v) {
-        return null;
+        Node node = nodeMap.get(v);
+        return node.adjacent;
     }
 
     /** Returns the Euclidean distance between vertices v and w, where Euclidean distance
-     *  is defined as sqrt( (lonV - lonV)^2 + (latV - latV)^2 ). */
+     *  is defined as sqrt( (lonV - lonW)^2 + (latV - latW)^2 ). */
     double distance(long v, long w) {
-        return 0;
+        Node node1 = this.nodeMap.get(v);
+        Node node2 = this.nodeMap.get(w);
+        return Math.sqrt((node1.lon - node2.lon) * (node1.lon - node2.lon)
+                + (node1.lat - node2.lat) * (node1.lat - node2.lat));
+    }
+    double distance2(long v, double lon, double lat) {
+        Node node1 = this.nodeMap.get(v);
+        return Math.sqrt((node1.lon - lon) * (node1.lon - lon)
+                + (node1.lat - lat) * (node1.lat - lat));
     }
 
     /** Returns the vertex id closest to the given longitude and latitude. */
     long closest(double lon, double lat) {
-        return 0;
+        long posMin = nodeList.get(1);
+        double dist1 = distance2(posMin, lon, lat);
+        for (int i = 1; i < nodeList.size(); i += 1) {
+            double dist2 = distance2(nodeList.get(i), lon, lat);
+            if (dist2 < dist1) {
+                dist1 = dist2;
+                posMin = nodeList.get(i);
+            }
+        }
+        return posMin;
     }
 
     /** Longitude of vertex v. */
     double lon(long v) {
-        return 0;
+        Node node1 = this.nodeMap.get(v);
+        return node1.lon;
     }
 
     /** Latitude of vertex v. */
     double lat(long v) {
-        return 0;
+        Node node1 = this.nodeMap.get(v);
+        return node1.lat;
     }
 }
